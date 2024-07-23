b2b32c16853cb355ac97db35b4883384cfac75b5
clarissaD <physicscla@gmail.com>
2024-07-22 23:52:19 -0300

    medias

--- MediaNotas.c
+++ MediaNotas.c
@@ -1,18 +1,1 @@
 #include <stdio.h>
-
-int main()
-{
-
-    float peso = 3.2; //criar variavel 15
-    float *ponteiro;
-    
-    ponteiro = &peso;
-    
-    printf("conteudo = %f\n", peso); 
-    printf("endereco de var = %p\n", &peso); //A forma correta para imprimir um ponteiro é o %p.
-    printf("endereco do ponteiro = %p\n", &ponteiro );
-
-    printf("Hello World!\n");
-
-    return 0;
-}
