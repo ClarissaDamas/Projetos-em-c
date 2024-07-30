e82de8cdcdfa81ad2e238c84d721a9c4e06eee8b
clarissaD <physicscla@gmail.com>
2024-07-30 17:19:17 -0300

    usando duas funcoes em C

--- codigoteste(usp).c
+++ codigoteste(usp).c
@@ -2,7 +2,7 @@
 #include <stdlib.h> //Contém funções para conversão de números, alocação de memória 
 //nao consigo executar as duas funcoes, elas separadamente funcionam, mas juntas da erro
 
-void main() {
+void alfabeto() {
 
     char c;
     c = 's';
@@ -10,23 +10,25 @@ void main() {
     printf(" %c", c);
     scanf(" %c/n",&c);
 
-    return 0
 }
 
 
-void main() {
+void somando() {
 
     unsigned char j = 255;
-    int a = 2, b;
+    //int a = 2, b;
     j = j + 10;
-    b = a++ //a++ primeiro o B assume o valor de a, e depois spma
-    b = ++a //++a primeiro soma o valor de a, e esse vai ser o novo valor de b
+    //b = a++ //a++ primeiro o B assume o valor de a, e depois spma
+    //b = ++a //++a primeiro soma o valor de a, e esse vai ser o novo valor de b
     printf("%u", j);
-
-    a ? b : c //se a for verdade vai acontecer b, senao vai acontecer c 
-    
   
 }
 
+void main() {
+
+    somando();
+    alfabeto();
+
+}
 
  // fim main
