23fe16b72d66917a126d201f489d59dc08641d24
clarissaD <physicscla@gmail.com>
2024-09-11 15:20:07 -0300

    Update teste.c
    Prox aula vamos fazer um codigo usando alocacao de memoria

--- teste.c
+++ teste.c
@@ -14,3 +14,10 @@ int main()
     scanf("%d",&x);
     pedirnumero(x);
 }
+
+/* variaveis dinamicas crescem como uma pilha inversa, ou seja crescem para baixo. Enquanto que as locais sao uma pilha de execucao que cresce para cima
+video sobre alocacao de memoria para python https://www.youtube.com/watch?v=Iwe3AULLFHM
+Para ter alocacao dinamica eu uso MALLOC ou CALLOC, isso vai criar uma variavel que pode ser eliminada depois usando FREE
+se eu nao tenho espaco disponivel ele vai retornar NULL, se tiver ele vai indicar o espaco
+*/
+
