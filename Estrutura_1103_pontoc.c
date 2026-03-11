1bd53fd15f03f5d412971c1fb492872c8968adfe
clarissaD <physicscla@gmail.com>
2026-03-11 11:37:39 -0300

    Fix include directive and add point functions

--- Estrutura_1103_pontoc.c
+++ Estrutura_1103_pontoc.c
@@ -1,6 +1,6 @@
 //cap 4 Arquivo Ponto c
 
-#incluse <stdlib.h>
+#include <stdlib.h>
 #include <math.h>
 #include "Ponto.h" //inclui os prototipos
 struct ponto{//definicao do tipo de dados
@@ -19,5 +19,43 @@ if (p != NULL) {
 return p;
 }
 
+//Destruindo um ponto
+
+void Ponto_libera (Ponto* p) {
+  free (p);
+}
+
+//Acessando conteudo de um ponto
+
+int Ponto_acessa (Ponto* p, float* x, float* y) {
+  if (p == NULL)
+  return 0;
+  *x = p -> x;
+  *y = p -> y;
+  return 1;
+}
+
+//atribuindo um valor ao ponto 
+
+int Ponto_atribui (Ponto* p, float x, float y) {
+  if(p == NULL)
+  return 0;
+  p -> x = x;
+  p -> y = y;
+  return 1;
+}
+
+//calculando a distancia dos pontos
+
+float Ponto_distancia (Ponto* p1, Ponto* p2){  //por que float e nao int como os outros?
+  if (p1 == NULL || p2 == NULL) //por que nao tem ponto e virgula?
+    return -1;
+
+  float dx = p1 -> x - p2 -> x;
+  float dy  = p1 -> y - p2 -> y; 
+
+  return sqrt(dx * dx + dy * dy);
+}
+    
 
 
